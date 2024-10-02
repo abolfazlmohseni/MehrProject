60355186cdb0cfb22a6be94e682184f5e9786d62
abolfazlmohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-10-02 20:07:47 +0330

    2/10/2024 commit 1

--- dashbord/js/script.js
+++ dashbord/js/script.js
@@ -30,5 +30,42 @@ BtnSizeMenu.addEventListener('click', function () {
         BtnSizeMenu.classList.remove("closemenu");
     }
 });
+// Charts
+
+var BarChart = document.getElementById("Bar-Chart").getContext("2d");
+var BarChartone = new Chart(BarChart, {
+  type: 'bar',
+  data: {
+    labels: ['1/1','1/2','1/3','1/4','1/5','1/6','1/7','1/8','1/9','1/10'], //دیتای تستی
+    datasets: [
+      {
+        label: 'رطوبت خاک',
+        data: [16, 83, 88, 26, 93, 25, 36, 61, 87, 63],    // دیتای تستی
+        backgroundColor: 'rgba(13, 110, 253, 0.3)',
+        borderColor: '#7A89FF',
+        borderWidth: 3,
+        hoverBorderWidth: 3.5,
+      },
+      {
+        label: 'دما',
+        data: [22, 65, 72, 55, 80, 45, 60, 70, 85, 78],    // دیتای تستی
+        backgroundColor: 'rgba(253, 110, 13, 0.3)',
+        borderColor: '#FF7A89',
+        borderWidth: 3,
+        hoverBorderWidth: 3.5,
+      }
+    ]
+  },
+  options: {
+    responsive: false,
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom'
+      }
+    }
+  }
+});
+
 
 
