23f93a66663374babf790475c4903258e9e5c5bb
abolfazlmohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-10-03 20:52:15 +0330

    3/10/2024 commit 1

--- dashbord/js/script.js
+++ dashbord/js/script.js
@@ -10,49 +10,53 @@ let FlagMenu = true
 // Menu size
 
 BtnSizeMenu.addEventListener('click', function () {
-    if (FlagMenu === false) {
-        menuElem.style.width = "2.5%";
-        mainSection.style.width = "97.5%";
-        FlagMenu = true;
-        ContentTextElem.forEach(function (item) {
-            item.classList.add("hidden");
-        });
-        nameCompanyElem.classList.add("hidden");
-        BtnSizeMenu.classList.add("closemenu");
-    } else {
-        menuElem.style.width = "12%";
-        mainSection.style.width = "88%";
-        FlagMenu = false;
-        ContentTextElem.forEach(function (item) {
-            item.classList.remove("hidden");
-        });
-        nameCompanyElem.classList.remove("hidden");
-        BtnSizeMenu.classList.remove("closemenu");
-    }
+  if (FlagMenu === false) {
+    menuElem.style.width = "2.5%";
+    mainSection.style.width = "97.5%";
+    FlagMenu = true;
+    ContentTextElem.forEach(function (item) {
+      item.classList.add("hidden");
+    });
+    nameCompanyElem.classList.add("hidden");
+    BtnSizeMenu.classList.add("closemenu");
+  } else {
+    menuElem.style.width = "12%";
+    mainSection.style.width = "88%";
+    FlagMenu = false;
+    ContentTextElem.forEach(function (item) {
+      item.classList.remove("hidden");
+    });
+    nameCompanyElem.classList.remove("hidden");
+    BtnSizeMenu.classList.remove("closemenu");
+  }
 });
 // Charts
-
 var BarChart = document.getElementById("Bar-Chart").getContext("2d");
+
+// ایجاد گرادینت برای آمار 1
+var gradient1 = BarChart.createLinearGradient(0, 0, 0, 400);
+gradient1.addColorStop(0, 'rgb(9, 182, 109)');   // رنگ بالا
+gradient1.addColorStop(1, 'rgb(37, 209, 232)');   // رنگ پایین
+
+// ایجاد گرادینت برای آمار 2
+var gradient2 = BarChart.createLinearGradient(0, 0, 0, 400);
+gradient2.addColorStop(0, 'rgb(255, 138, 72)');   // رنگ بالا
+gradient2.addColorStop(1, 'rgb(253, 191, 94)');   // رنگ پایین
+
 var BarChartone = new Chart(BarChart, {
   type: 'bar',
   data: {
-    labels: ['1/1','1/2','1/3','1/4','1/5','1/6','1/7','1/8','1/9','1/10'], //دیتای تستی
+    labels: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'], // دیتای تستی
     datasets: [
       {
-        label: 'رطوبت خاک',
-        data: [16, 83, 88, 26, 93, 25, 36, 61, 87, 63],    // دیتای تستی
-        backgroundColor: 'rgba(13, 110, 253, 0.3)',
-        borderColor: '#7A89FF',
-        borderWidth: 3,
-        hoverBorderWidth: 3.5,
+        label: 'آمار 1',
+        data: [213, 54, 96, 153, 456, 245, 355],  // دیتای تستی
+        backgroundColor: gradient1
       },
       {
-        label: 'دما',
-        data: [22, 65, 72, 55, 80, 45, 60, 70, 85, 78],    // دیتای تستی
-        backgroundColor: 'rgba(253, 110, 13, 0.3)',
-        borderColor: '#FF7A89',
-        borderWidth: 3,
-        hoverBorderWidth: 3.5,
+        label: 'آمار 2',
+        data: [175, 123, 466, 485, 174, 453, 125],  // دیتای تستی
+        backgroundColor: gradient2                // استفاده از گرادینت برای آمار 2
       }
     ]
   },
@@ -60,12 +64,19 @@ var BarChartone = new Chart(BarChart, {
     responsive: false,
     scales: {
       x: {
-        type: 'linear',
-        position: 'bottom'
+        ticks: {
+          font: {
+            family: 'iransans',  // تنظیم خانواده فونت برای محور X
+            size: 14,         // اندازه فونت
+            style: 'italic',  // سبک فونت (italic)
+            weight: 'bold',   // وزن فونت (bold)
+            lineHeight: 1.2   // فاصله خطوط
+          }
+        }
       }
     }
   }
-});
+}); 
 
 
 
