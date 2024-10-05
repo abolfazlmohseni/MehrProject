ad7664aeb840353e03a094689c287bf04689f83c
abolfazlmohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-10-05 19:06:56 +0330

    commit

--- dashbord/js/script.js
+++ dashbord/js/script.js
@@ -33,30 +33,29 @@ BtnSizeMenu.addEventListener('click', function () {
 // Charts
 var BarChart = document.getElementById("Bar-Chart").getContext("2d");
 
-// ایجاد گرادینت برای آمار 1
-var gradient1 = BarChart.createLinearGradient(0, 0, 0, 400);
-gradient1.addColorStop(0, 'rgb(9, 182, 109)');   // رنگ بالا
-gradient1.addColorStop(1, 'rgb(37, 209, 232)');   // رنگ پایین
 
-// ایجاد گرادینت برای آمار 2
+var gradient1 = BarChart.createLinearGradient(0, 0, 0, 400);
+gradient1.addColorStop(0, 'rgb(9, 182, 109)');  
+gradient1.addColorStop(1, 'rgb(37, 209, 232)');   
+
 var gradient2 = BarChart.createLinearGradient(0, 0, 0, 400);
-gradient2.addColorStop(0, 'rgb(255, 138, 72)');   // رنگ بالا
-gradient2.addColorStop(1, 'rgb(253, 191, 94)');   // رنگ پایین
+gradient2.addColorStop(0, 'rgb(255, 138, 72)');   
+gradient2.addColorStop(1, 'rgb(253, 191, 94)');   
 
 var BarChartone = new Chart(BarChart, {
   type: 'bar',
   data: {
-    labels: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'], // دیتای تستی
+    labels: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه'],
     datasets: [
       {
         label: 'آمار 1',
-        data: [213, 54, 96, 153, 456, 245, 355],  // دیتای تستی
+        data: [213, 54, 96, 153, 456, 245, 355], 
         backgroundColor: gradient1
       },
       {
         label: 'آمار 2',
-        data: [175, 123, 466, 485, 174, 453, 125],  // دیتای تستی
-        backgroundColor: gradient2                // استفاده از گرادینت برای آمار 2
+        data: [175, 123, 466, 485, 174, 453, 125], 
+        backgroundColor: gradient2           
       }
     ]
   },
@@ -66,11 +65,11 @@ var BarChartone = new Chart(BarChart, {
       x: {
         ticks: {
           font: {
-            family: 'iransans',  // تنظیم خانواده فونت برای محور X
-            size: 14,         // اندازه فونت
-            style: 'italic',  // سبک فونت (italic)
-            weight: 'bold',   // وزن فونت (bold)
-            lineHeight: 1.2   // فاصله خطوط
+            family: 'iransans',  
+            size: 14,  
+            style: 'italic',  
+            weight: 'bold',   
+            lineHeight: 1.2   
           }
         }
       }
